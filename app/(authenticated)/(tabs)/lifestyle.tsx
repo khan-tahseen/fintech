f1b276deb790ccb0388cf98dfd0a2e334e569baa
khan-tahseen <tasheenkhanit@gmail.com>
2024-04-06 05:36:05 +0530

    authenticated tabs added and verified successFully

---
+++ app/(authenticated)/(tabs)/lifestyle.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Lifestyle = () => {
+  return (
+    <View>
+      <Text>Lifestyle</Text>
+    </View>
+  )
+}
+
+export default Lifestyle
+
+const styles = StyleSheet.create({})
