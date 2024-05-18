4dddb8db1b3fade31e3718b22cb081102e849462
khan-tahseen <tasheenkhanit@gmail.com>
2024-05-18 17:07:52 +0530

    done with lock screen functionality

--- app/(authenticated)/(modals)/lock.tsx
+++ app/(authenticated)/(modals)/lock.tsx
@@ -4,9 +4,11 @@ import { useUser } from '@clerk/clerk-expo';
 import * as Haptics from 'expo-haptics';
 import Colors from '@/constants/Colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 
 const lock = () => {
   const { user } = useUser();
+  const router = useRouter()
   const [firstName, setFirstName] = useState(user?.firstName || '');
   const [code, setCode] = useState<number[]>([]);
   const codeLength = Array(6).fill(0);
@@ -14,6 +16,12 @@ const lock = () => {
   useEffect(() => {
     if (code.length === 6) {
       console.log('code => ', code);
+      if (code.join('') === '123456') {
+        router.push('/(authenticated)/(tabs)/home');
+      } else {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+        setCode([]);
+      }
     }
   }, [code]);
 
@@ -67,16 +75,20 @@ const lock = () => {
         </View>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
             <TouchableOpacity style={{padding: 12}} onPress={onBiometricAuthPress}>
-              <MaterialCommunityIcons name='face-recognition' size={24} color='#000' />
+              <MaterialCommunityIcons name='face-recognition' size={26} color='#000' />
             </TouchableOpacity>
 
             <TouchableOpacity style={{padding: 12}} onPress={() => onNumberPress(0)}>
               <Text style={styles.numbers}>0</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={{padding: 12}} onPress={() => onNumberPress(0)}>
-              <Text style={styles.numbers}>0</Text>
+          <View style={{minWidth: 30}}>
+            {code.length > 0 && (
+            <TouchableOpacity style={{padding: 12}} onPress={numberBackspace}>
+              <MaterialCommunityIcons name='backspace-outline' size={26} color='#000' />
             </TouchableOpacity>
+            )}
+          </View>
         </View>
       </View>
     </SafeAreaView>
