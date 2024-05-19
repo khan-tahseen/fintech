2aab005c91d0575bc4e344c2bc2d82f400436e2f
khan-tahseen <tasheenkhanit@gmail.com>
2024-05-19 21:57:29 +0530

    done with profile section

--- app/(authenticated)/(modals)/account.tsx
+++ app/(authenticated)/(modals)/account.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useAuth, useUser } from '@clerk/clerk-expo';
 import { BlurView } from 'expo-blur';
 import Colors from '@/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
 import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -13,16 +14,38 @@ const Account = () => {
     const [lastName, setLastName] = useState(user?.lastName);
     const [edit, setEdit] = useState(false);
 
-    const onCaptureImage = () => {
-        // Handle image capture logic here
-        console.log('inside capture image..')
+    const onCaptureImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 0.75,
+            base64: true,
+        });
+
+        if (!result.canceled) {
+            const base64 = `data:image/png;base64,${result.assets[0].base64}`;
+            user?.setProfileImage({ file: base64 });
+        }
+    }
+
+    const onSaveUser = async () => {
+        try {
+            await user?.update({
+                firstName: firstName!,
+                lastName: lastName!,
+            })
+            setEdit(false)
+        } catch (error) {
+            console.log('error saving user', error)
+        }
     }
 
     return (
         <BlurView intensity={60} tint='dark' style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)', paddingTop: 80 }}>
             <View style={{ alignItems: 'center' }}>
                 <TouchableOpacity onPress={onCaptureImage} style={styles.captureBtn}>
-                    {user?.imageUrl && <Image />}
+                    {user?.imageUrl && <Image source={{uri: user?.imageUrl}} style={styles.avatar} />}
                 </TouchableOpacity>
 
                 {user && (
@@ -51,13 +74,35 @@ const Account = () => {
                                     onChangeText={setLastName}
                                     style={styles.inputField}
                                 />
-                                <TouchableOpacity onPress={() => setEdit(true)}>
-                                    <Ionicons name='ellipsis-horizontal' size={24} color='#fff' />
+                                <TouchableOpacity onPress={onSaveUser}>
+                                    <Ionicons name='checkmark-outline' size={24} color='#fff' />
                                 </TouchableOpacity>
                             </View>
                         )}
                     </View>
                 )}
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.btn} onPress={() => console.log('Logout user')}>
+                        <Ionicons name='log-out' size={24} color='#fff' />
+                        <Text style={{color: '#fff', fontSize: 18}}>Log out</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => console.log('Account')}>
+                        <Ionicons name='person' size={24} color='#fff' />
+                        <Text style={{color: '#fff', fontSize: 18}}>Account</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => console.log('Learn')}>
+                        <Ionicons name='bulb' size={24} color='#fff' />
+                        <Text style={{color: '#fff', fontSize: 18}}>Learn</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={() => console.log('Inbox')}>
+                        <Ionicons name='megaphone' size={24} color='#fff' />
+                        <Text style={{color: '#fff', fontSize: 18}}>Inbox</Text>
+                        <View style={styles.notify}>
+                            <Text style={{color: '#fff'}}>14</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </BlurView>
     );
@@ -73,9 +118,9 @@ const styles = StyleSheet.create({
         gap: 12
     },
     captureBtn: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: 130,
+        height: 130,
+        borderRadius: 70,
         backgroundColor: Colors.gray,
         justifyContent: 'center',
         alignItems: 'center',
@@ -90,6 +135,33 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         padding: 10,
         textAlign: 'center'
+    },
+    avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 70
+    },
+    actions: {
+        backgroundColor: 'rgba(256, 256, 256, 0.1)',
+        borderRadius: 12,
+        gap: 2,
+        margin: 18,
+        width: 320
+    },
+    btn: {
+        padding: 12,
+        gap: 18,
+        flexDirection: 'row'
+    },
+    notify: {
+        backgroundColor: Colors.primary,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        justifyContent: 'center',
+        position: 'absolute',
+        right: 8,
+        top: 16,
+        padding: 4
     }
 
 });
